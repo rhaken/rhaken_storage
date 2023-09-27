fde3a5fffb47fd09ca1d23d5280212f1868a6c79
rhaken <rhakenss@gmail.com>
2023-09-27 10:11:03 +0700

    tugas selesai

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id 
+from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id
+from main.views import register #sesuaikan dengan nama fungsi yang dibuat
+from main.views import login_user #sesuaikan dengan nama fungsi yang dibuat
+from main.views import logout_user, add_amount, sub_amount, delete_item
+
 
 app_name = 'main'
 
@@ -9,5 +13,11 @@ urlpatterns = [
     path('xml/', show_xml, name='show_xml'), 
     path('json/', show_json, name='show_json'),
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
-    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
+    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
+    path('register/', register, name='register'), #sesuaikan dengan nama fungsi yang dibuat
+    path('login/', login_user, name='login'), #sesuaikan dengan nama fungsi yang dibuat
+    path('logout/', logout_user, name='logout'),
+    path('add-amount/<int:id>/',add_amount, name='add_amount'),
+    path('sub-amount/<int:id>/',sub_amount, name='sub_amount'),
+    path('delete_item/<int:id>/',delete_item, name='delete_item'),
 ]
