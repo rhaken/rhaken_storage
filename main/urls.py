21ddcb226ccfc11298745e8b6d91f821a98c5af3
rhaken <rhakenss@gmail.com>
2023-11-22 02:50:55 +0700

    selesai tugas 9

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id
 from main.views import register #sesuaikan dengan nama fungsi yang dibuat
 from main.views import login_user #sesuaikan dengan nama fungsi yang dibuat
-from main.views import logout_user, add_amount, sub_amount, delete_item, get_product_json, add_product_ajax
+from main.views import logout_user, add_amount, sub_amount, delete_item, get_product_json, add_product_ajax, create_product_flutter
 
 
 app_name = 'main'
@@ -22,4 +22,5 @@ urlpatterns = [
     path('delete_item/<int:id>/',delete_item, name='delete_item'),
     path('get-product/', get_product_json, name='get_product_json'),
     path('create-product-ajax/', add_product_ajax, name='add_product_ajax'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
 ]
